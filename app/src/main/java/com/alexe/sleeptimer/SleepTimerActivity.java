fc264856927185a17f8d251be9adbd3ad32243a2
AlexREvans <AlexREvans@users.noreply.github.com>
2016-05-07 09:17:26 +0000

    second

--- app/src/main/java/com/alexe/sleeptimer/SleepTimerActivity.java
+++ app/src/main/java/com/alexe/sleeptimer/SleepTimerActivity.java
@@ -13,10 +13,24 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.TimeUnit;
+
 public class SleepTimerActivity extends AppCompatActivity {
 
     private TextView timeRemaining;
     private RadioGroup radios;
+
+    private final String REMAINING_TIME = "REMAINING TIME";
+    private long remainingTime = 0;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save the user's current game state
+        savedInstanceState.putLong(REMAINING_TIME, remainingTime);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,12 @@ public class SleepTimerActivity extends AppCompatActivity {
 
         timeRemaining = (TextView) findViewById(R.id.countdown);
         radios = (RadioGroup) findViewById(R.id.radios);
+
+
+        if(savedInstanceState != null) {
+            remainingTime = savedInstanceState.getLong(REMAINING_TIME);
+            startCountdown(remainingTime);
+        }
     }
 
     public void onRadioTimeClicked(View view) {
@@ -34,36 +54,42 @@ public class SleepTimerActivity extends AppCompatActivity {
         long timeSeconds = 0;
 
         switch (view.getId()) {
-            case R.id.radioButton5s:
-                timeSeconds = 5;
-                break;
-            case R.id.radioButton30s:
-                timeSeconds = 30;
-                break;
             case R.id.radioButton5m:
                 timeSeconds = 5 * 60;
                 break;
             case R.id.radioButton10m:
                 timeSeconds = 10 * 60;
                 break;
+            case R.id.radioButton15m:
+                timeSeconds = 15 * 60;
+                break;
             case R.id.radioButton30m:
                 timeSeconds = 30 * 60;
                 break;
         }
 
-        startCountdown(timeSeconds);
+        startCountdown(timeSeconds * 1000);
     }
 
-    private void startCountdown(long countdownSeconds) {
-        new CountDownTimer(countdownSeconds * 1000, 1000) {
+    private void startCountdown(long countdownMillis) {
+        new CountDownTimer(countdownMillis, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                timeRemaining.setText("seconds remaining: " + millisUntilFinished / 1000);
+
+                String timer = String.format("Time remaining - %02d:%02d",
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))
+                );
+
+                remainingTime = millisUntilFinished;
+                timeRemaining.setText(timer);
             }
 
             public void onFinish() {
                 radios.clearCheck();
                 timeRemaining.setText("Done!");
+                remainingTime = 0;
 
                 sendMediaButton(getApplicationContext(), KeyEvent.KEYCODE_MEDIA_PAUSE);
 
